b4480764e697e353c16c2abf0b4b22646797bb28
dbto1970 <donbartojunior@gmail.com>
2021-12-17 13:41:27 -0500

    looking in to slow server response

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import postRoutes from './routes/posts.js';
 const app = express();
 
 
-app.use(express.json({ limit: "30mb", extended: true}));
-app.use(express.urlencoded({ limit: "30mb", extended: true}));
+app.use(express.json({ limit: "30", extended: true}));
+app.use(express.urlencoded({ limit: "30", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 
